32e0b7b0f9a68ed6054bf957785676bef2392ddd
Wesley <wspencer3295@yahoo.com>
2021-01-13 13:22:19 -0700

    commit tree e67a73889bed2223dfe7bbe30003b29cd992e91b

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"] }
+    validate :non_clickbait
+
+    CLICKBAIT = [/Won't Believe/, /Secret/, /Top [0-9]/, /Guess/]
+
+    def non_clickbait
+        
+        if CLICKBAIT.none?{|click|  click.match title}
+
+            errors.add(:title, "clickbaity")
+        end
+    end
 end
---
+++ app/validators/post_validator.rb
@@ -0,0 +1,4 @@
+class PostValidator < ActiveModel::Validator
+
+
+end
